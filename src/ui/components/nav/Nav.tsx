0d3281eb5983237b1d0ac53b7d9a9b6f397b4c51
unknown <midouyahya22@gmail.com>
2025-07-16 18:17:19 +0000

    Fix: Remove unused MobileMenu import to resolve Vercel build error

--- src/ui/components/nav/Nav.tsx
+++ src/ui/components/nav/Nav.tsx
@@ -2,14 +2,10 @@ import { Suspense } from "react";
 
 import { CartNavItem } from "./components/CartNavItem";
 
-import { MobileMenu } from "./components/MobileMenu";
-
 export const Nav = ({ channel }: { channel: string }) => {
 	return (
 		<nav className="flex w-full gap-4 lg:gap-6" aria-label="Main navigation">
-			<ul className="hidden gap-4 overflow-x-auto whitespace-nowrap md:flex lg:gap-8 lg:px-0">
-				
-			</ul>
+			<ul className="hidden gap-4 overflow-x-auto whitespace-nowrap md:flex lg:gap-8 lg:px-0"></ul>
 			<div className="ml-auto flex items-center justify-center gap-4 whitespace-nowrap lg:gap-8">
 				<div className="hidden lg:flex"></div>
 				<svg width="20" height="20" viewBox="0 0 24 23" fill="none" xmlns="http://www.w3.org/2000/svg">
